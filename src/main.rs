5f45b70bc7fa7b57296c6b608ee6f8d5cf189cac
RiseAndCry <riseandcry@gmail.com>
2021-04-04 10:45:14 +0300

    add collision system

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@
 
 use bevy::{
     prelude::*,
+    sprite::collide_aabb::{collide, Collision},
 };
 
 // todo handle resizing, different resolutions
 const SCREEN_WIDTH: f32 = 1280.0;
 const SCREEN_HEIGHT: f32 = 720.0;
-const SPELL_VELOCITY: f32 = 200.0;
+const SPELL_VELOCITY: f32 = 400.0;
 
 struct Spell {
     velocity: Vec3,
 }
 
+#[derive(PartialEq)]
+enum Collider {
+    Solid,
+}
 
 fn main() {
     App::build()
@@ -25,6 +30,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(spell_movement_system.system())
+        .add_system(spell_collision_system.system())
         .run();
 }
 
@@ -49,7 +55,8 @@ fn spawn_arena_bounds(commands: &mut Commands, materials: &mut ResMut<Assets<Col
         transform: Transform::from_translation(Vec3::new(0.0, bounds.y / 2.0, 0.0)),
         sprite: Sprite::new(Vec2::new(bounds.x + wall_thickness, wall_thickness)),
         ..Default::default()
-    });
+    })
+    .with(Collider::Solid);
 
     // right
     commands.spawn(SpriteBundle {
@@ -57,7 +64,8 @@ fn spawn_arena_bounds(commands: &mut Commands, materials: &mut ResMut<Assets<Col
         transform: Transform::from_translation(Vec3::new(bounds.x / 2.0, 0.0, 0.0)),
         sprite: Sprite::new(Vec2::new(wall_thickness, bounds.y + wall_thickness)),
         ..Default::default()
-    });
+    })
+    .with(Collider::Solid);
 
     // bottom
     commands.spawn(SpriteBundle {
@@ -65,7 +73,8 @@ fn spawn_arena_bounds(commands: &mut Commands, materials: &mut ResMut<Assets<Col
         transform: Transform::from_translation(Vec3::new(0.0, -bounds.y / 2.0, 0.0)),
         sprite: Sprite::new(Vec2::new(bounds.x + wall_thickness, wall_thickness)),
         ..Default::default()
-    });
+    })
+    .with(Collider::Solid);
 
     // left
     commands.spawn(SpriteBundle {
@@ -73,10 +82,12 @@ fn spawn_arena_bounds(commands: &mut Commands, materials: &mut ResMut<Assets<Col
         transform: Transform::from_translation(Vec3::new(-bounds.x / 2.0, 0.0, 0.0)),
         sprite: Sprite::new(Vec2::new(wall_thickness, bounds.y + wall_thickness)),
         ..Default::default()
-    });
+    })
+    .with(Collider::Solid);
 }
 
 fn spawn_spell(commands: &mut Commands, materials: &mut ResMut<Assets<ColorMaterial>>) {
+
     commands
         .spawn(SpriteBundle {
             material: materials.add(Color::rgb(1.0, 0.5, 0.5).into()),
@@ -95,5 +106,51 @@ fn spell_movement_system(time: Res<Time>, mut spell_query: Query<(&Spell, &mut T
 
     for (spell, mut transform) in spell_query.iter_mut() {
         transform.translation += spell.velocity * delta_seconds;
+    }
+}
+
+fn spell_collision_system(
+    mut spell_query: Query<(&mut Spell, &Transform, &Sprite)>,
+    collider_query: Query<(Entity, &Collider, &Transform, &Sprite)>,
+) {
+    for (mut spell, spell_transform, sprite) in spell_query.iter_mut() {
+        let spell_size = sprite.size;
+        let spell_velocity = &mut spell.velocity;
+
+        for (_, collider, transform, sprite) in collider_query.iter() {
+            // collision is not detected if the object is moving too fast, so either it has to be
+            // slower, or one of the colliding objects needs to be bigger in size.
+            // https://github.com/bevyengine/bevy/issues/1240
+            let collision = collide(
+                spell_transform.translation,
+                spell_size,
+                transform.translation,
+                sprite.size,
+            );
+            if let Some(collision) = collision {
+                let mut reflect_x = false;
+                let mut reflect_y = false;
+
+                match collision {
+                    Collision::Left => reflect_x = spell_velocity.x > 0.0,
+                    Collision::Right => reflect_x = spell_velocity.x < 0.0,
+                    Collision::Top => reflect_y = spell_velocity.y < 0.0,
+                    Collision::Bottom => reflect_y = spell_velocity.y > 0.0,
+                }
+
+                if reflect_x {
+                    spell_velocity.x = -spell_velocity.x;
+                }
+
+                if reflect_y {
+                    spell_velocity.y = -spell_velocity.y;
+                }
+
+                // break if this collide is on a solid, otherwise continue check whether a solid is also in collision
+                if *collider == Collider::Solid {
+                    break;
+                }
+            }
+        }
     }
 }
