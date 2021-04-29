4868c0440fdc929fb8cca4f6786aae4312e9b942
RiseAndCry <riseandcry@gmail.com>
2021-04-29 09:12:22 +0300

    Quit the app upon pressing "Quit" button in main menu

--- src/systems/ui/main_menu.rs
+++ src/systems/ui/main_menu.rs
@@ -1,21 +1,29 @@
 #![warn(clippy::pedantic)]
 
 use crate::prelude::*;
+use bevy::app::AppExit;
 
 pub fn main_menu_system(
     mut state: ResMut<State<AppState>>,
     button_materials: Res<ButtonMaterials>,
+    menu_data: Res<MenuData>,
+    mut exit: EventWriter<AppExit>,
     mut interaction_query: Query<
-        (&Interaction, &mut Handle<ColorMaterial>),
+        (Entity, &Interaction, &mut Handle<ColorMaterial>),
         (Changed<Interaction>, With<Button>),
     >,
 ) {
-    // todo quit upon clicking `QUIT`
-    for (interaction, mut material) in interaction_query.iter_mut() {
+    for (entity, interaction, mut material) in interaction_query.iter_mut() {
         match *interaction {
             Interaction::Clicked => {
                 *material = button_materials.pressed.clone();
-                state.set(AppState::InGame).unwrap();
+                // todo why does match not work here ?
+                if entity.id() == menu_data.play_button_entity.id() {
+                    state.set(AppState::InGame).unwrap();
+                }
+                if entity.id() == menu_data.quit_button_entity.id() {
+                    exit.send(AppExit);
+                }
             }
             Interaction::Hovered => {
                 *material = button_materials.hovered.clone();
