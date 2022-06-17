d73809f02c03d9b38be72d18457687bc33a3ea74
RiseAndCry <riseandcry@gmail.com>
2022-06-17 13:47:52 +0300

    Add Font constants for simpler loading

--- src/setup/mod.rs
+++ src/setup/mod.rs
@@ -3,6 +3,7 @@ pub mod ui;
 pub use crate::setup::ui::main_menu::*;
 pub use crate::setup::ui::health_text::*;
 pub use crate::setup::ui::game_over_text::*;
+pub use crate::setup::ui::fonts::*;
 
 mod map_builder;
 mod player;
